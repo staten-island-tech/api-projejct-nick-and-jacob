bed82bd6bf5ac81220937941fabcb36269975200
Nick <113193039+NickFromSiths@users.noreply.github.com>
2023-01-12 11:00:28 -0500

    .

--- api/flaskr/data.py
+++ api/flaskr/data.py
@@ -2,17 +2,17 @@ data = {'card':
     [{'option':'Search Country',
      'info':'',
      'link':'/Search_Country',
-     'img':'',},
+     'image':'',},
     
     {'option':'World Map',
      'info':'',
      'link':'/World_Map',
-     'img':'',},
+     'image':'../static/imgs/PngItem_588664.png',},
     
     {'option':'Country List',
      'info':'',
      'link':'/Country_List',
-     'img':'',},
+     'image':'',},
     ],
     'intworld': 'Welcome to Interactive World Data'
 }
