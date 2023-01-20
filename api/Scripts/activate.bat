6a9885a5bd28a8e3143c2220466e99b8e265763d
NickFromSiths <113193039+NickFromSiths@users.noreply.github.com>
2023-01-19 19:04:34 -0500

    .

--- api/Scripts/activate.bat
+++ api/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\SunFlower\Documents\GitHub\api-projejct-nick-and-jacob\api
+set VIRTUAL_ENV=C:\Users\Nicholas.Malyarchuk\Documents\GitHub\api-projejct-nick-and-jacob\api
 
 if not defined PROMPT set PROMPT=$P$G
 
